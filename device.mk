ce50f1e2834fcfce41a8377025068fc104d89f63
0xKANARIA <enashinonome264@gmail.com>
2025-09-13 08:12:10 +0000

    Revert "vince: remove fingerprint permission temporarily to be safe"
    This reverts commit f89fbcff07f7d95bca65faf75e61b5a08f8a77cc.

--- device.mk
+++ device.mk
@@ -44,6 +44,9 @@ PRODUCT_PACKAGES += \
 PRODUCT_PACKAGES += \
     liblzma.vendor:64
 
+PRODUCT_COPY_FILES += \
+    frameworks/native/data/etc/android.hardware.fingerprint.xml:$(TARGET_COPY_OUT_VENDOR)/etc/permissions/android.hardware.fingerprint.xml
+
 # Input
 PRODUCT_COPY_FILES += \
     $(call find-copy-subdir-files,*,$(LOCAL_PATH)/keylayout/,$(TARGET_COPY_OUT_VENDOR)/usr/keylayout/)
