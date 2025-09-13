f4b30615b337cc8ea47bc522ece8da001f39dc2b
0xKANARIA <enashinonome264@gmail.com>
2025-09-13 08:15:26 +0000

    Revert "vince: biometrics: Disable fingerprint on failure"
    This reverts commit a1fa907655fd9d5db0c45a38b164f7e38d0d8e27.

--- biometrics/BiometricsFingerprint.cpp
+++ biometrics/BiometricsFingerprint.cpp
@@ -16,8 +16,6 @@
 #define LOG_TAG "android.hardware.biometrics.fingerprint@2.1-service.xiaomi_vince"
 #define LOG_VERBOSE "android.hardware.biometrics.fingerprint@2.1-service.xiaomi_vince"
 
-#include <android-base/properties.h>
-
 #include <hardware/hw_auth_token.h>
 
 #include <hardware/hardware.h>
@@ -50,7 +48,6 @@ BiometricsFingerprint::BiometricsFingerprint() : mClientCallback(nullptr), mDevi
     mDevice = openHal();
     if (!mDevice) {
         ALOGE("Can't open HAL module");
-        android::base::SetProperty("ro.vendor.fingerprint.failed", "1");
     }
 }
 
