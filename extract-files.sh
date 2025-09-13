2a10f68f51f9a81e3cb78cf566e115634b6e4af3
0xKANARIA <enashinonome264@gmail.com>
2025-09-13 08:11:42 +0000

    vince: Also patch fingerprint blobs to use v32

--- extract-files.sh
+++ extract-files.sh
@@ -8,7 +8,7 @@
 
 function blob_fixup() {
     case "${1}" in
-        vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so)
+        vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0.so | vendor/lib64/com.fingerprints.extension@1.0.so)
             "${PATCHELF}" --replace-needed "libhidlbase.so" "libhidlbase-v32.so" "${2}"
             ;;
         vendor/lib64/libvendor.goodix.hardware.fingerprint@1.0-service.so)
