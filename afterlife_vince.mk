3786c2c01774c0a578ab6ec94520b74dbe1328e6
mizukanaAlt <rina301087@gmail.com>
2025-11-10 22:27:52 +0000

    vince: go official afterlife :v

--- afterlife_vince.mk
+++ afterlife_vince.mk
@@ -9,18 +9,21 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/core_64_bit.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 $(call inherit-product, $(SRC_TARGET_DIR)/product/product_launched_with_n_mr1.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common AfterLife stuff
+$(call inherit-product, vendor/afterlife/config/common_full_phone.mk)
 
 # Inherit from vince device
 $(call inherit-product, device/xiaomi/vince/device.mk)
+
+# AfterLife flags
+AFTERLIFE_MAINTAINER := RenzAlt
 
 # Kernel
 TARGET_KERNEL_VERSION := 4.19
 
 # Device identifier. This must come after all inclusions
 PRODUCT_DEVICE := vince
-PRODUCT_NAME := lineage_vince
+PRODUCT_NAME := afterlife_vince
 PRODUCT_BRAND := Xiaomi
 PRODUCT_MODEL := Redmi 5 Plus
 PRODUCT_MANUFACTURER := Xiaomi
