835648bedb4e40eacf31ead10c6eda3cb7caa9f0
Mika <enashinonome264@gmail.com>
2026-07-27 17:01:23 +0000

    vince: update Mi8953 unified kernel configs

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -20,9 +20,10 @@ TARGET_SCREEN_DENSITY := 440
 DEVICE_MANIFEST_FILE += $(DEVICE_PATH)/manifest.xml
 
 # Kernel
+TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 TARGET_KERNEL_CONFIG := \
         vendor/msm8953-perf_defconfig \
-        vendor/xiaomi/vince.config
+        vendor/mi8953.config
 TARGET_KERNEL_SOURCE := kernel/xiaomi/msm8953
 
 # Partitions
